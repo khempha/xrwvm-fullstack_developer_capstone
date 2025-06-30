247f4fcae6589b941a5d1d7d106bf3004268fc2e
khemPha <pha.khem@amkcambodia.com>
2025-06-30 13:44:00 +0700

    model 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -107,7 +107,6 @@ def get_cars(request):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
-# ...
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if(dealer_id):
@@ -115,12 +114,11 @@ def get_dealer_reviews(request, dealer_id):
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
-            print(review_detail)
-            # review_detail['sentiment'] = response['sentiment']
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
-
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
 # ...
--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -6,8 +6,7 @@ import positive_icon from "../assets/positive.png"
 import neutral_icon from "../assets/neutral.png"
 import negative_icon from "../assets/negative.png"
 import review_icon from "../assets/reviewbutton.png"
-import Header from '../Header/Header';
-
+import Header from '../Header/Header'
 const Dealer = () => {
 
 
@@ -56,15 +55,18 @@ const Dealer = () => {
     return icon;
   }
 
-  useEffect(() => {
-    get_dealer();
-    get_reviews();
-    if(sessionStorage.getItem("username")) {
-      setPostReview(<a href={post_review}><img src={review_icon} style={{width:'10%',marginLeft:'10px',marginTop:'10px'}} alt='Post Review'/></a>)
 
-      
-    }
-  },[]);  
+  useEffect(() => {
+  get_dealer();
+  get_reviews();
+  if(sessionStorage.getItem("username")) {
+    setPostReview(
+      <a href={post_review}>
+        <img src={review_icon} style={{width:'10%', marginLeft:'10px', marginTop:'10px'}} alt='Post Review'/>
+      </a>
+    );
+  }
+}, []);
 
 
 return(
